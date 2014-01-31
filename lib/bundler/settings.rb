a4bbaa09410451ab9d68fa2bda3bd93435b1bf17
Kashyap <kashyap.kmbc@gmail.com>
2014-01-31 18:19:59 +0530

    Fix string interpolation issue
    * Using '' to substitute `\n` characters will introduce unwanted
     characters which can be mitigated by using "" inside the String#gsub
     method.

--- lib/bundler/settings.rb
+++ lib/bundler/settings.rb
@@ -155,7 +155,7 @@ module Bundler
       valid_file = config_file && config_file.exist? && !config_file.size.zero?
       if !ignore_config? && valid_file
         config_regex =/^(BUNDLE_.+): (?:['"](.*)['"]|(.+(?:\n(?!BUNDLE).+))|(.+))$/
-        config_pairs = config_file.read.scan(config_regex).map{|m| m.compact.map { |n| n.gsub(/\n/, '') } }
+        config_pairs = config_file.read.scan(config_regex).map{|m| m.compact.map { |n| n.gsub(/\n/, "") } }
         Hash[config_pairs]
       else
         {}
